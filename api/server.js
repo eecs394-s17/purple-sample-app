beb420698fa55c80e0adbe2b0faf83c9a5e925a7
Safia Abdalla <safia@safia.rocks>
2017-03-30 15:06:30 -0500

    Add basic API wrapper

---
+++ api/server.js
@@ -0,0 +1,41 @@
+const Hapi = require('hapi');
+const server = new Hapi.Server();
+const printers = require('./printer-data.js');
+
+server.connection({
+  host: 'localhost',
+  port: '8000',
+});
+
+server.route({
+  method: 'GET',
+  path: '/printers',
+  handler: function(request, reply) {
+    return reply(printers);
+  }
+});
+
+server.route({
+  method: 'GET',
+  path: '/printers/active',
+  handler: function(request, reply) {
+    return reply(printers.filter(function(printer) {
+      return printer.active;
+    }));
+  }
+});
+
+server.route({
+  method: 'GET',
+  path: '/printers/{id}',
+  handler: function(request, reply) {
+    return reply(printers.filter(function(printer) {
+      return printer.id === parseInt(request.params.id);
+    }));
+  }
+});
+
+server.start(function(error) {
+  if (error) throw error;
+  console.log('API server is running at: ', server.info.uri);
+});
