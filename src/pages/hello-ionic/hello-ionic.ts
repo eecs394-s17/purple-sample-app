785fe0a7b30f4b61da28ca8959343281c30ec281
Wajihuddin <wajihuddinmohammed2018@u.northwestern.edu>
2017-03-31 12:31:22 -0500

    Added default hardcoded marker

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
- 
+
 declare var google;
 
 @Component({
@@ -17,9 +17,20 @@ export class HelloIonicPage {
         this.loadMap();
     }
 
+    addMarker(location, map) {
+    /** Add the marker at the clicked location, and add the next-available label
+        from the array of alphabetical characters.*/
+      let marker = new google.maps.Marker({
+        position: location,
+        map: this.map,
+      });
+    return marker;
+    }
+
     loadMap(){
         let latLng = new google.maps.LatLng(42.052936, -87.679330);
 
+        let marker = this.addMarker(latLng, this.map)
         let mapOptions = {
             center: latLng,
             zoom: 15,
@@ -27,5 +38,7 @@ export class HelloIonicPage {
         }
 
         this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+        marker.setMap(this.map);
     }
+
 }
