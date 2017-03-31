202986cba16bb6f387d20c396439878c668dd453
Safia Abdalla <safia@safia.rocks>
2017-03-31 10:20:14 -0500

    Render Google Map on home page

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NavController } from 'ionic-angular';
+ 
+declare var google;
 
 @Component({
   selector: 'page-hello-ionic',
   templateUrl: 'hello-ionic.html'
 })
 export class HelloIonicPage {
-  constructor() {
+    @ViewChild('map') mapElement: ElementRef;
+    map: any;
 
-  }
+    constructor(public navCtrl: NavController) {}
+
+    ionViewDidLoad(){
+        this.loadMap();
+    }
+
+    loadMap(){
+        let latLng = new google.maps.LatLng(-87.679330, 42.052936);
+
+        let mapOptions = {
+            center: latLng,
+            zoom: 15,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+
+        this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+    }
 }
